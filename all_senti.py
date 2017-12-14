feddc5e3756e906b1693a9bfe815f8a129fa0073
Sabina Talibova <sabinatalibov1@gmail.com>
2017-12-14 06:53:28 -0800

    working prototype

--- all_senti.py
+++ all_senti.py
@@ -1,10 +1,69 @@
 
 import csv
-inputTweets=csv.reader(open('data.csv','r'),delimiter=',')
-#featureList[]
+import nltk
+import re
+
+def processTweet(tweet):
+	tweet=tweet.lower()
+	tweet=re.sub('idi',' ',tweet)
+	tweet=re.sub('dir',' ',tweet)
+	tweet=re.sub('em',' ',tweet)
+	return tweet
+
+def getFeatureVector(tweet):
+	
+	featureVector=[]
+	
+
+
+	words=tweet.split(" ")
+	
+	
+	for w in words:
+	
+		featureVector.append(w.lower())
+	return featureVector
+
+def extract_features(tweet):
+    tweet_words = set(tweet)
+    features = {}
+    for word in featureList:
+        features['contains(%s)' % word] = (word in tweet_words)
+    return features
+
+
+
+
+
+
+inputTweets=csv.reader(open('data.csv','r'),delimiter=";")
+featureList=[]
 
 tweets=[]
 for row in inputTweets:
-	sentiment=row[3]
 	tweet=row[0]
+	sentiment=row[2]
 	
+	
+	
+	processedTweet=processTweet(tweet)
+	
+	featureVector=getFeatureVector(processedTweet)
+	featureList.extend(featureVector)
+
+	tweets.append((featureVector,sentiment))
+
+
+featureList=list(set(featureList))
+#print(featureList)
+
+training_set=nltk.classify.util.apply_features(extract_features,tweets)
+#print(training_set)
+NBClassifier=nltk.NaiveBayesClassifier.train(training_set)
+
+testTweet='razi deyiləm'
+processed=processTweet(testTweet)
+result=NBClassifier.classify(extract_features(getFeatureVector(processed)))
+print(result)
+
+
--- processTweets.py
+++ processTweets.py
@@ -1,25 +1,16 @@
 import re
 
-def processTweets(tweet):
-	tweet=tweet.lower()
-	tweet=re.sub('idi',' ',tweet)
-	tweet=re.sub('.dir$',' ',tweet)
-	tweet=re.sub('em',' ',tweet)
-	print(tweet)
-
 
 
 	
-
-
-filetoprocess=open('data.txt','r')
-line=filetoprocess.readline()
-
-
-while line:
 	
-	processed=processTweets(line)
-	
-	line=filetoprocess.readline()
 
-filetoprocess.close()
+
+filetoprocess=open('data1.txt','r')
+line=filetoprocess.readline().split(",")
+
+
+while filetoprocess:
+	
+	print(line[0])
+
