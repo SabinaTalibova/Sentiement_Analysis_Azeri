200591bf32c6e478eac69dc69b6cfb3c7fd47991
Sabina Talibova <sabinatalibov1@gmail.com>
2017-12-21 06:50:33 -0800

    done.

--- all_senti.py
+++ all_senti.py
@@ -1,31 +1,23 @@
 
 import csv
 import nltk
-import re
+
 import tkinter as tk
 from tkinter import *
-import time
-#import tkfont 
+
 from tkinter import font
 from tkinter import messagebox
+import pickle 
 
 
+def processSentence(sentenece):
+	sentenece=sentenece.lower()
+	return sentenece
 
-def processTweet(tweet):
-	tweet=tweet.lower()
-
-	#tweet=re.sub('idi',' ',tweet)
-	tweet=re.sub('dir',' ',tweet)
-	#tweet=re.sub('em',' ',tweet)
-	return tweet
-
-def getFeatureVector(tweet):
+def getFeatureVector(sentenece):
 	
 	featureVector=[]
-	
-
-
-	words=tweet.split(" ")
+	words=sentenece.split(" ")
 	
 	
 	for w in words:
@@ -33,42 +25,36 @@ def getFeatureVector(tweet):
 		featureVector.append(w.lower())
 	return featureVector
 
-def extract_features(tweet):
-    tweet_words = set(tweet)
+def extract_features(sentenece):
+    sentenece_words = set(sentenece)
     features = {}
     for word in featureList:
-        features['contains(%s)' % word] = (word in tweet_words)
+        features['contains(%s)' % word] = (word in sentenece_words)
     return features
-
-
-
-
-
-
 
 def featurelistAndArray():
     featureList=[]
-    inputTweets=csv.reader(open('data.csv','r'),delimiter=",")
+    inputsenteneces=csv.reader(open('data.csv','r'),delimiter=",")
 
 
-    tweets=[]
-    for row in inputTweets:
-	    tweet=row[0]
+    senteneces=[]
+    for row in inputsenteneces:
+	    sentenece=row[0]
 	    sentiment=row[2]
 	
 	
-	    processedTweet=processTweet(tweet)
+	    processedsentenece=processSentence(sentenece)
 	 
-	    featureVector=getFeatureVector(processedTweet)
+	    featureVector=getFeatureVector(processedsentenece)
 	    featureList.extend(featureVector)
 
-	    tweets.append((featureVector,sentiment))
+	    senteneces.append((featureVector,sentiment))
 
 
     featureList=list(set(featureList))
-    return (featureList,tweets)
+    return (featureList,senteneces)
 featureList=featurelistAndArray()[0]
-tweets=featurelistAndArray()[1]
+senteneces=featurelistAndArray()[1]
 
 
 def show():
@@ -79,15 +65,16 @@ def show():
     top.geometry("%dx%d+0+0" % (RWidth, RHeight))
 
 
-    L1 = Label(top, text="Enter the sentnce you want to check",font=("Helvetica", 20))
+    L1 = Label(top, text="Enter the sentence you want to check",font=("Helvetica", 20))
     L1.place(x=90, y=50)
     E1 = Entry(top, bd =3)
     E1.place(x=130, y=150,width=400,height=50)
     def get():
-    	testTweet=E1.get()
-    	processed=processTweet(testTweet)
-    	training_set=nltk.classify.util.apply_features(extract_features,tweets)
-    	NBClassifier=nltk.NaiveBayesClassifier.train(training_set)
+    	testsentenece=E1.get()
+    	processed=processSentence(testsentenece)
+    	classifier_f=open("naive.pickle","rb")
+    	NBClassifier=pickle.load(classifier_f)
+    	classifier_f.close()
     	result=NBClassifier.classify(extract_features(getFeatureVector(processed)))
     	
 
@@ -113,25 +100,13 @@ def show():
 
 
     def info():
-    	messagebox.showinfo("Information",'Used Algorithm: Naive Bayes\nTraining Data set: 1629 sentneces\nTest Data set: 100 \nAccuracy of algorithm: 82%\nSource Code and Documentation: sabina.github.io')
+    	messagebox.showinfo("Information",'Used Algorithm:	Naive Bayes\nTraining Data set:	1629 sentneces\nTest Data set:	100 senteneces\nAccuracy of algorithm: 82%\nSource Code and Documentation: http://bit.ly/2oVbKJU')
     
+
     img=tk.PhotoImage(file="info.png")
     B3=Button(top,text="info",command=info,image=img,borderwidth=0)
     B3.place(x=580, y=0,width=150,height=50)
     top.mainloop() 
-
-
-show()
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -139,11 +114,11 @@ def accuracychecker():
 	counter=0
 	inputTest=csv.reader(open('TestData.csv','r'))
 	for row in inputTest:
-		tweet=row[0]
+		sentenece=row[0]
 		sentiment=row[3]
-		testTweet=tweet
-		processed=processTweet(testTweet)
-		training_set=nltk.classify.util.apply_features(extract_features,tweets)
+		testsentenece=sentenece
+		processed=processSentence(testsentenece)
+		training_set=nltk.classify.util.apply_features(extract_features,senteneces)
 		NBClassifier=nltk.NaiveBayesClassifier.train(training_set)
 		result=NBClassifier.classify(extract_features(getFeatureVector(processed)))
 		if result==sentiment:
@@ -151,6 +126,21 @@ def accuracychecker():
 		else:
 			counter=counter
 	print(counter)
+
+
+#main
+show()
+
+
+
+
+'''training_set=nltk.classify.util.apply_features(extract_features,senteneces)
+
+NBClassifier=nltk.NaiveBayesClassifier.train(training_set)
+
+save_classifier=open("naive.pickle","wb")
+pickle.dump(NBClassifier,save_classifier)
+save_classifier.close()'''
 
 
 
